6850dad165a71fad5f5956093daaa31ff17ab286
koo <kookh0987@gmail.com>
2017-06-29 21:13:02 +0900

    handle range excel

--- Com.py
+++ Com.py
@@ -4,6 +4,8 @@ excel=win32com.client.Dispatch("Excel.Application")
 excel.Visible=True
 wb=excel.Workbooks.Open('C:\\Users\\kookh\\Desktop\\test.xlsx')
 ws=wb.ActiveSheet
-print(ws.Cells(1,1).Value)
 
-excel.Quit()
+ws.Cells(1, 2).Value="is"
+ws.Range("C1").Value="good"
+#ws.Range("C1").Interior.ColorIndex=10
+ws.Range("A2:C1").Interior.ColorIndex=27
